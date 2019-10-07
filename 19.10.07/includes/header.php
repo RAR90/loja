76d105b919ddc6aa573cc06d260ec9843d4893b3
RAR90 <rafaelribeiro.email@gmail.com>
2019-10-07 14:45:06 -0300

    19.10.07

--- 19.10.07/includes/header.php
+++ 19.10.07/includes/header.php
@@ -6,7 +6,6 @@
 <html lang="pt">
 <head>
   <title><?=$company;?></title>
-
   <script type="application/ld+json">
     {
       "@context": "http://schema.org",
@@ -20,7 +19,6 @@
       }]
     }
   </script>
-
   <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
   <meta name="revisit-after" content="7 days" />
@@ -32,23 +30,17 @@
   <meta name="title" content="" />
   <link rel="shortcut icon" href="imgs/favicon.ico?<?=rand();?>" type="image/x-icon" />
   <link rel="icon" href="imgs/favicon.ico?<?=rand();?>" type="image/x-icon" />
-
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-   <style type="text/css">
+  <style type="text/css">
     <?php
       include('includes/css/style.min.css');
       include('includes/temas/'.$theme.'.css');
       include('includes/css/custom.css');
     ?>
   </style>
-
   <?php include('includes/theme.php'); ?>
-
-
-
 </head>
 <body class="body <?=$theme;?>">
 <?php
@@ -58,7 +50,6 @@
   include('includes/cart-bar.php');
 
   if ($onScrollBar == 'alternative') include('includes/on-scroll-bar.php');
-
   switch ($headerType) {
     case 'header-address':
       include('includes/header-address.php');
